ff94b59604f9bc6f2c65c39db87fa6cd1c0dfe12
Marc Mengel <mengel@fnal.gov>
2025-08-05 14:48:02 -0500

    Make libxml2 default True again, as this is the upstream default.

--- repos/spack_repo/builtin/packages/python/package.py
+++ repos/spack_repo/builtin/packages/python/package.py
@@ -269,7 +269,7 @@ class Python(Package):
     extendable = True
 
     # Variants to avoid cyclical dependencies for concretizer
-    variant("libxml2", default=False, description="Use a gettext library build with libxml2")
+    variant("libxml2", default=True, description="Use a gettext library build with libxml2")
 
     variant(
         "debug", default=False, description="debug build with extra checks (this is high overhead)"
