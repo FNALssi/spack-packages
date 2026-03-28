a4e565d90cd5d6f47cb7993c4d8e64d84a838c11
Patrick Gartung <gartung@fnal.gov>
2026-03-27 19:23:23 -0500

    Add pandorasdk@4.1.0

--- repos/spack_repo/builtin/packages/pandorasdk/package.py
+++ repos/spack_repo/builtin/packages/pandorasdk/package.py
@@ -20,6 +20,7 @@ class Pandorasdk(CMakePackage):
     maintainers("jmcarcell", "wdconinc")
 
     version("master", branch="master")
+    version("4.1.0", sha256="30f544c7f8981f40e7544e004db8d67bd2399a3c067a4010da3b012182c6fd25")
     version("4.0.2", sha256="9c8e051dbfd3be711dc7940658b78558277e2ec1c6305c0f7c7bb271abd3e4a8")
     version("3.4.2", sha256="e076adb2e3d28d3ac5dcc06bcc6e96815d23ef7782e1a87842b1e3e96e194994")
     version("3.4.1", sha256="9607bf52a9d79d88d28c45d4f3336e066338b36ab81b4d2d125226f4ad3a7aaf")
