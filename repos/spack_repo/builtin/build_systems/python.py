19a687d43a1606d0043fdb27a571a7d79fd1e04f
Marc Mengel <mengel@fnal.gov>
2026-09-24 15:44:01 -0500

    just one fix

--- repos/spack_repo/builtin/build_systems/python.py
+++ repos/spack_repo/builtin/build_systems/python.py
@@ -211,7 +211,7 @@ def _homepage(cls: "PythonPackage") -> Optional[str]:
 
 
 def _urls(cls: "PythonPackage") -> Optional[str]:
-    if cls.pypi and not hasattr(cls, 'url') and not hasattr(cls, 'sourceforge_mirror_path'):
+    if cls.pypi and not hasattr(cls, 'url'):
         # handle both older mixed case and recent lowercased package urls
         url = _url(cls)
         urls = [url.lower(), url]
